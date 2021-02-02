9d0080c09deb7305fd77606c318c376afb259b08
IRRT <zindgg@gmail.com>
2021-02-02 10:45:34 +0300

    Create ConfigurableApplicationContext "end"

--- src/main/java/com/irrt/spring/springduble/controller/HomeController.java
+++ src/main/java/com/irrt/spring/springduble/controller/HomeController.java
@@ -2,7 +2,11 @@ package com.irrt.spring.springduble.controller;
 
 import com.irrt.spring.springduble.model.Book;
 import com.irrt.spring.springduble.repo.BookRepository;
+import com.irrt.spring.springduble.service.SoundAnimals;
+import net.bytebuddy.build.Plugin;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,14 +18,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     private BookRepository bookRepository;
+    private SoundAnimals soundAnimals;
 
     @Autowired
-    public HomeController(BookRepository bookRepository) {
+    public HomeController(BookRepository bookRepository, //@Qualifier("catSound")
+            SoundAnimals soundAnimals) {
         this.bookRepository = bookRepository;
+        this.soundAnimals = soundAnimals;
     }
+
 
 @GetMapping
     public String getIndex(Model model){
+    System.out.println("From Controler: " + soundAnimals.sound());
         model.addAttribute("books", bookRepository.findAll());
        model.addAttribute("newbook", new Book());
         return "index";
