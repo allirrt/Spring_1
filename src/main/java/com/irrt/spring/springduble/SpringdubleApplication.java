5ac9f0ee06fde868b40eef08cd962c8ebf2a0845
IRRT <zindgg@gmail.com>
2021-02-02 10:10:11 +0300

    Create ConfigurableApplicationContext

--- src/main/java/com/irrt/spring/springduble/SpringdubleApplication.java
+++ src/main/java/com/irrt/spring/springduble/SpringdubleApplication.java
@@ -1,13 +1,21 @@
 package com.irrt.spring.springduble;
 
+import com.irrt.spring.springduble.service.CatSound;
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.applet.AppletContext;
 
 @SpringBootApplication
 public class SpringdubleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringdubleApplication.class, args);
+
+        ConfigurableApplicationContext ctx =  SpringApplication.run(SpringdubleApplication.class, args);
+        CatSound catSound =(CatSound) ctx.getBean("catSound");
+        System.out.println(catSound.sound());
     }
 
 }
---
+++ src/main/java/com/irrt/spring/springduble/service/SoundAnimals.java
@@ -0,0 +1,5 @@
+package com.irrt.spring.springduble.service;
+
+public interface SoundAnimals {
+    String sound();
+}
